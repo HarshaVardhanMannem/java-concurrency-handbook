678bed02064c02ced48a9defa4f0ad4fc6924834
Harsha Vardhan <hmannem@uab.edu>
2026-05-09 17:01:42 -0500

    test: add initial test scaffold for Main class

---
+++ newsradar/src/test/java/com/newsradar/MainTest.java
@@ -0,0 +1,13 @@
+package com.newsradar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class MainTest {
+
+    @Test
+    void mainRunsWithoutThrowing() {
+        assertDoesNotThrow(() -> Main.main(new String[]{}));
+    }
+}
